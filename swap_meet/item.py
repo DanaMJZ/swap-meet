5c28f3da24d516719dda646a74e7b271d62f6f72
Ellie Alavi <elhamalavi.f@gmail.com>
2025-03-31 19:42:58 -0400

    Wave 02 added

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
 class Item:
-    pass
+
+    def __init__(self, id=None):
+        # self.id = id if id is not None else uuid.uuid4().int
+        self.id = uuid.uuid4().int if id is None else id
+
+    def get_category(self):
+        return "Item"
+    
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,3 +2,18 @@ class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory if inventory is not None else []
 
+    def add(self,item):
+        inventory = self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            inventory = self.inventory.remove(item)
+            return item
+        return False
+    
+    def get_by_id(self,id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None  # Return None if no match is found
