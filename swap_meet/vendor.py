6c7f24b084aa45334b34c1d7b518acc366a67c07
Dana <da.alshaar@gmail.com>
2025-04-01 12:33:35 -0700

    test wave3 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,3 +17,15 @@ class Vendor:
             if item.id == id:
                 return item
         return None  # Return None if no match is found
+    
+    def swap_items(self, other_vendor, my_item, their_item): # test fatimah.swap_items(jolie, item_b, item_d) python automatically provide self.
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+    
+        self.inventory.remove(my_item)
+        other_vendor.inventory.remove(their_item)
+    
+        self.inventory.append(their_item)
+        other_vendor.inventory.append(my_item)
+    
+        return True
