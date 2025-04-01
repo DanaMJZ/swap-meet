6436ffb8751a05462877a7f29ad07f012c0925a7
Ellie Alavi <elhamalavi.f@gmail.com>
2025-04-01 15:53:32 -0400

    Wave04 Completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -16,4 +16,24 @@ class Vendor:
         for item in self.inventory:
             if item.id == id:
                 return item
-        return None  # Return None if no match is found
+        return None         # Returns None if no match is found
+    
+    def swap_item(vendor, item1,item2):
+        pass
+
+
+    def swap_first_item(self,vendor): 
+        #swaps first item of vendor arguement, with first item of inventory of the instance vendor calling the method
+        
+        inventory1 = vendor.inventory
+        inventory2 = self.inventory
+
+        if not inventory1 or not inventory2:
+            return False
+        
+        helper = inventory1[0]
+        inventory1[0] = inventory2[0]
+        inventory2[0] = helper
+
+        return inventory2
+
